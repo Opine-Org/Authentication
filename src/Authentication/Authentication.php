d08bced4fab28d2ae76f44ee55b89fcd24f2fd9d
webuser <ryan@virtuecenter.com>
2013-10-20 01:27:11 -0400

    update

--- src/Authentication/Authentication.php
+++ src/Authentication/Authentication.php
@@ -6,13 +6,39 @@ class Authentication {
 	private $separation;
 	private $response;
 
-	public function __construct ($db, $separation, $response) {
+	public function __construct ($db, $config, $separation, $response) {
 		$this->db = $db;
 		$this->separation = $separation;
 		$this->response = $response;
 	}
 
-	public function valid () {
+	public function valid ($name) {
+		if (!isset($_SESSION['auth']) { {
+			return false;
+		}
+		if (!isset($_SESSION['auth'][$name]) {
+			return false;
+		}
+		if (!isset($_SESSION['auth'][$name]['status'])) {
+			return false;
+		}
+		if ($_SESSION['auth'][$name]['status'] !== true) {
+			return false;
+		}
 		return true;
+	}
+
+	public function login ($name) {
+		$db->collection('users')->findOne(
+
+		);
+	}
+
+	public function passwordForgot () {
+
+	}
+
+	public function passwordChange () {
+
 	}
 }
