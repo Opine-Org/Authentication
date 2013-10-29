ef6c31834a656bc7ceed39b77a74630f4a79b62b
webuser <ryan@virtuecenter.com>
2013-10-29 11:00:21 -0400

    login, logout, hash, acl are verified

--- src/Authentication/Authentication.php
+++ src/Authentication/Authentication.php
@@ -26,38 +26,139 @@ namespace Authentication;
 
 class Authentication {
 	private $db;
+	private $config;
 
-	public function __construct ($db) {
+	public function __construct ($db, $config) {
 		$this->db = $db;
+		$this->config = $config;
 	}
 
-	public function valid ($name) {
+	public function valid ($zone) {
 		if (!isset($_SESSION['auth'])) {
 			return false;
 		}
-		if (!isset($_SESSION['auth'][$name])) {
+		if (!isset($_SESSION['auth'][$zone])) {
 			return false;
 		}
-		if (!isset($_SESSION['auth'][$name]['status'])) {
+		if (!isset($_SESSION['auth'][$zone]['status'])) {
 			return false;
 		}
-		if ($_SESSION['auth'][$name]['status'] !== true) {
+		if ($_SESSION['auth'][$zone]['status'] !== true) {
 			return false;
 		}
-		return $_SESSION['auth'][$name]['_id'];
+		return $_SESSION['auth'][$zone]['_id'];
 	}
 
-	public function login ($name) {
-		$db->collection('users')->findOne(
-
-		);
+	public function login ($zone, $identity, $password, $identityField='email') {
+//FIXME: rate limit...
+		if ($identityField == 'email') {
+			$identity = trim(strtolower($identity));
+		}
+		$user = $this->db->collection('users')->findOne(
+			[
+				$identityField => $identity,
+				'password' => $this->passwordHash($password),
+				'acl.zone' => $zone
+			], [
+				'_id', 
+				'email', 
+				'first_name', 
+				'last_name', 
+				'acl', 
+				'created_date',
+				'image'
+			]);
+		if (!isset($user['_id'])) {
+			return false;
+		}
+		$access = false;
+		$aclZone = [];
+		foreach ($user['acl'] as $acl) {
+			if ($acl['zone'] != $zone) {
+				continue;
+			}
+			if (!isset($acl['acl']) || !is_array($acl['acl']) || count($acl['acl']) == 0) {
+				continue;
+			}
+			$access = true;
+			$aclZone = $acl;
+			break;
+		}
+		if ($access === false) {
+			return false;
+		}
+		if (!isset($_SESSION['auth'])) {
+			$_SESSION['auth'] = [];
+		}
+		$_SESSION['auth'][$zone] = [];
+		$_SESSION['auth'][$zone]['status'] = true;
+		$_SESSION['auth'][$zone]['_id'] = (string)$user['_id'];
+		$_SESSION['auth'][$zone]['acl'] = $aclZone;
+		$_SESSION['auth'][$zone]['user'] = $user;
+		$this->db->collection('login_history')->save([
+			'user_id' => $user['_id'],
+			'created_date' => new \MongoDate(strtotime('now')),
+			'zone' => $zone
+		]);
+		return true;
 	}
 
-	public function passwordForgot () {
-
+	public function whoami ($zone) {
+		if (!isset($_SESSION['auth'])) {
+			$_SESSION['auth'] = [];
+			return false;
+		}
+		if (!isset($_SESSION['auth'][$zone])) {
+			return false;
+		}
+		if (!isset($_SESSION['auth'][$zone]['user'])) {
+			return false;
+		}
+		return $_SESSION['auth'][$zone]['user'];
 	}
 
-	public function passwordChange () {
+	public function permission ($zone, $access) {
+		if (!isset($_SESSION['auth'])) {
+			$_SESSION['auth'] = [];
+			return false;
+		}
+		if (!isset($_SESSION['auth'][$zone])) {
+			return false;
+		}
+		if (!isset($_SESSION['auth'][$zone]['acl'])) {
+			return false;
+		}
+		if (in_array('superadmin', $_SESSION['auth'][$zone]['acl'])) {
+			return true;
+		}
+		if (in_array($access, $_SESSION['auth'][$zone]['acl'])) {
+			return true;
+		}
+		return false;
+	}
 
+	public function logout ($zone) {
+		if (!isset($_SESSION['auth'])) {
+			$_SESSION['auth'] = [];
+		}
+		$_SESSION['auth'][$zone] = [];
+		return true;
+	}
+
+	private function passwordHash ($password) {
+		$config = $this->config->auth;
+		return sha1($config['salt'] . $password);
+	}
+
+	public function passwordForgot ($email) {
+		//validate user
+		//enforce rate limit
+		//generate token
+		//email via topic
+	}
+
+	public function passwordChange ($id, $token, $password) {
+		//validate token
+		//change password, remove token
 	}
 }
